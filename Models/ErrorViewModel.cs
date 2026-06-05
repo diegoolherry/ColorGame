83e6013ca15a2c7ba354d902895ed7e0f4299c6f
Diego Olherry <diegoolherry2407@gmail.com>
2026-06-05 15:04:08 -0300

    Remove duplicate old code from root after merge

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ColorGame.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- Models/PlayerStat.cs
+++
@@ -1,9 +0,0 @@
-namespace ColorGame.Models
-{
-    public class PlayerStat
-    {
-        public string Name { get; set; } = "";
-        public double AccumulatedSeconds { get; set; } = 0;
-        public List<string> Colors { get; set; } = new();
-    }
-}
